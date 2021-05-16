452f348f0359e3a18ed3c9e8b8edfa36e3a95cdc
suraj220487 <49473141+suraj220487@users.noreply.github.com>
2021-05-16 20:19:25 +0530

    Project generated from Spring Intilizr

---
+++ src/main/java/com/db/gcp/example/gcppubsubclient/GcpPubsubClientApplication.java
@@ -0,0 +1,13 @@
+package com.db.gcp.example.gcppubsubclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcpPubsubClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcpPubsubClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/db/gcp/example/gcppubsubclient/GcpPubsubClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.db.gcp.example.gcppubsubclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcpPubsubClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
